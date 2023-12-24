c4378bf8f35310ed38d7f114d0f28a170761382b
Aleks B <aleksandir@me.com>
2023-12-24 23:55:14 +1030

    Add data_tools.py to load and find the product with the maximum price

---
+++ src/data_tools.py
@@ -0,0 +1,15 @@
+import json
+
+with open("data/scraped_data.json", "r") as f:
+    data = json.load(f)
+
+max_price = 0
+max_product = None
+
+for name, product in data.items():
+    if float(product["price"].strip("$").replace(",", "")) > float(max_price):
+        max_price = product["price"].strip("$").replace(",", "")
+        max_product = product
+
+print(max_product)
+# print(name, product)
