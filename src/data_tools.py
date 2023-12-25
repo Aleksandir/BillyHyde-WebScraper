2c793b2fdd0e878cd7ce8ae70b79879736387b63
Aleks B <aleksandir@me.com>
2023-12-25 20:09:52 +1030

    Refactor data_tools.py: Add functions for finding min price and total number of products

--- src/data_tools.py
+++ src/data_tools.py
@@ -9,7 +9,7 @@ def max_price():
     max_price = 0
     max_product = None
 
-    for name, product in data.items():
+    for _, product in data.items():
         if float(product["price"].strip("$").replace(",", "")) > float(max_price):
             max_price = product["price"].strip("$").replace(",", "")
             max_product = product
@@ -17,8 +17,34 @@ def max_price():
     return max_product
 
 
+def min_price():
+    with open("data/scraped_data.json", "r") as f:
+        data = json.load(f)
+
+    min_price = 1
+    min_product = None
+
+    for _, product in data.items():
+        if float(product["price"].strip("$").replace(",", "")) < float(min_price):
+            min_price = product["price"].strip("$").replace(",", "")
+            min_product = product
+
+    return min_product
+
+
+def total_number_of_products():
+    with open("data/scraped_data.json", "r") as f:
+        data = json.load(f)
+
+    return len(data)
+
+
 def main():
-    pprint.pprint(max_price())
+    pprint.pprint(f"highest price product: {max_price()}\n")
+
+    pprint.pprint(f"lowest price product: {min_price()}\n")
+
+    print(f"total number of products: {total_number_of_products()}")
 
 
 if __name__ == "__main__":
