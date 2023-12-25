1be37c8527ab2a9e34183b049eb6df1a6153004b
Aleks B <aleksandir@me.com>
2023-12-25 18:55:43 +1030

    Refactor max_price function in data_tools.py

--- src/data_tools.py
+++ src/data_tools.py
@@ -1,16 +1,21 @@
 import json
 import pprint
 
-with open("data/scraped_data.json", "r") as f:
-    data = json.load(f)
 
-max_price = 0
-max_product = None
+def max_price():
+    with open("data/scraped_data.json", "r") as f:
+        data = json.load(f)
 
-for name, product in data.items():
-    if float(product["price"].strip("$").replace(",", "")) > float(max_price):
-        max_price = product["price"].strip("$").replace(",", "")
-        max_product = product
+    max_price = 0
+    max_product = None
 
-print(pprint.pformat(max_product))
-# print(name, product)
+    for name, product in data.items():
+        if float(product["price"].strip("$").replace(",", "")) > float(max_price):
+            max_price = product["price"].strip("$").replace(",", "")
+            max_product = product
+
+    return max_product
+
+
+def main():
+    pprint.pprint(max_price())
