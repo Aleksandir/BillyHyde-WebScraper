b7beddb1cd5e4f4129455a888fdcabb18db4d1fa
Aleks B <aleksandir@me.com>
2023-12-25 20:26:10 +1030

    Add average_price() function to calculate the average price of products

--- src/data_tools.py
+++ src/data_tools.py
@@ -39,12 +39,27 @@ def total_number_of_products():
     return len(data)
 
 
+def average_price():
+    with open("data/scraped_data.json", "r") as f:
+        data = json.load(f)
+
+    total_price = 0
+
+    for _, product in data.items():
+        total_price += float(product["price"].strip("$").replace(",", ""))
+
+    return round(total_price / len(data), 2)
+
+
 def main():
+    # pretty print does not work with newlines, used print instead, continued for consistency
     pprint.pprint(f"highest price product: {max_price()}")
     print()
     pprint.pprint(f"lowest price product: {min_price()}")
     print()
     print(f"total number of products: {total_number_of_products()}")
+    print()
+    print(f"Average price: ${average_price()}")
 
 
 if __name__ == "__main__":
